5ddc1cbbbbc1d10cf18cb151606e174b3d03f1cd
unknown <epeng1994@gmail.com>
2023-01-05 17:57:10 -0500

    constructed apioptions and search function for Asyncpagination component

---
+++ src/components/Search/api.js
@@ -0,0 +1,10 @@
+export const options = {
+    method:'GET',
+    headers: {
+        'X-RapidAPI-Key':'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+
+export default options;
--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,23 +1,44 @@
 import { useState } from 'react';
 import { AsyncPaginate } from 'react-select-async-paginate';
-
+import axios from 'axios';
+import APIOptions from './api'
 
 const Search = ({onSearchChange}) => {
 
     const [search,setSearch] = useState(null);
+    const apiID = process.env.REACT_APP_weatherAPI || '99252094471af63f3bc4db3139381388'
 
     const handleOnChange = searchData =>{
         setSearch(searchData);
         onSearchChange(searchData)
     }
 
+    const loadOptions = async inputValue =>{
+        return await axios.get(`https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=1000000&namePrefix=${inputValue}`,APIOptions)
+            .then(res=>{
+                console.log(res.data.data)
+                return{
+                    options: res.data.data.map(city=>{
+                        return {
+                            value:`${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`
+                        }
+                    })
+                }
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    }
+
     return(
         <>
             <AsyncPaginate
-                placeholder = 'Search for city'
-                debounceTimeout = {600}
-                value = {search}
-                onChange = {handleOnChange}
+                placeholder='Search for city'
+                debounceTimeout={600}
+                value={search}
+                onChange={handleOnChange}
+                loadOptions={loadOptions}
             />
         </>
     )
