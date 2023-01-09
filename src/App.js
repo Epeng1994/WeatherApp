eb22e91bb9de79d3969b9688976cc9be4107cefc
unknown <epeng1994@gmail.com>
2023-01-09 14:54:21 -0500

    added additional state for fahrenheit/celcius boolean, removed additional API call

--- src/App.js
+++ src/App.js
@@ -7,43 +7,42 @@ import Search from './components/Search/search';
 
 function App() {
   const apiID = process.env.REACT_APP_weatherAPI || '99252094471af63f3bc4db3139381388'
-  const [currentWeather, setCurrentWeather] = useState(null)
-  const [forecast, setCurrentForecast] = useState([])
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setCurrentForecast] = useState([]);
+  const [isCelcius, setIsCelcius] = useState(true);
 
   const handleOnSearchChange = async searchData =>{
     const [latitude,longitude]  = searchData.value.split(' ')
 
-    await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&appid=${apiID}`)
-      .then(res=>{
-        setCurrentWeather(res.data)
-      })
-      .catch(err=>{
-        console.log(err)
-      })
-
     await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiID}&units=metric`)
     .then(res=>{
-      let weatherResult = res.data.list
-      setCurrentForecast([weatherResult[0],weatherResult[8],weatherResult[16],weatherResult[24],weatherResult[32]])
+      let weatherResult = res.data.list;
+      setCurrentWeather(weatherResult[0]);
+      setCurrentForecast([weatherResult[8],weatherResult[16],weatherResult[24],weatherResult[32]]);
     })
     .catch(err=>{
       console.log(err)
-    })
-      
+    });
   };
 
-  const calculateTempToFarenheit = temp =>{
-    return Math.round((temp - 273.15)* 9/5 + 32)
-  };
+  const CelciusToFarenheit = temp =>{
+    return Math.round((temp*9/5)+32)
+  }
+
+   const alterFarenheitAndCelcius = e =>{
+    setIsCelcius(!isCelcius);
+  }
 
   const resetWeather = e =>{
     setCurrentWeather(null);
     setCurrentForecast([]);
+    setIsCelcius(true);
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <button className = 'search-button' onClick = {()=>alterFarenheitAndCelcius()}>Change to {isCelcius ? 'Farenheit' : 'Celcius'}</button>
         <div>
           <img src='./assets/weather.gif' alt='weather gif' className = 'weatherHeader'/>
         </div>
@@ -51,14 +50,13 @@ function App() {
           <Search onSearchChange={handleOnSearchChange}/>
           <button className = 'search-button' onClick={resetWeather}>Clear Search</button>
         </div>
-        
       </header>
       <div className = 'container-row'>
-        {currentWeather && <CurrentWeather data = {currentWeather} calculateTempToFarenheit={calculateTempToFarenheit}></CurrentWeather>}
+        {currentWeather && <CurrentWeather data = {currentWeather} isCelcius = {isCelcius} CelciusToFarenheit={CelciusToFarenheit}></CurrentWeather>}
       </div>
       <div className = 'weatherFlex'>
         <div className = 'weatherContainer'>
-          <ForecastWeather data = {forecast} calculateTempToFarenheit={calculateTempToFarenheit}/>
+          <ForecastWeather data = {forecast} CelciusToFarenheit = {CelciusToFarenheit} isCelcius = {isCelcius}/>
         </div>
       </div>
       
